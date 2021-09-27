b393076eb228ae53e6685dd7f758ca59c46f5973
Aniruddh <ojhaa94@gmail.com>
2021-09-27 23:13:38 +0100

    links field inputs to app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('what up loser')
 
-    // const billValue = document.getElementById("bill-name").value
-
+    const billValue = document.getElementById("bill-input").value
+    const numOfPeople = document.getElementById("num-of-people").value
+    console.log("Value of the Bill = £" + billValue)
+    console.log("There are " + numOfPeople + " guests.")
     
 })
