dddaf1ac90cfd3acb293ca843c7fa41d920925d0
anioji94 <ojhaa94@gmail.com>
2021-10-03 18:11:48 +0100

    minor edits

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const numOfPeople = document.getElementById("num-of-people").value
     console.log("Value of the Bill = £" + billValue)
     console.log("There are " + numOfPeople + " guests.")
+
+    document.getElementById("5").onclick = () => {
+        console.log((billValue*(0.05))/numOfPeople)
+    }
+    document.getElementById("10")
+    document.getElementById("15")
+    document.getElementById("25")
+    document.getElementById("50")
     
 })
