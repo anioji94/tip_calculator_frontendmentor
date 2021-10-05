5c72a5d30d8d359bd4c3b22272d0fdf87c84615f
Aniruddh <ojhaa94@gmail.com>
2021-10-05 19:25:43 +0100

    focusing on element triggers function

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', (event) => {
  const bill = document.getElementById('bill-input')
- const tip = document.getElementById('percentage')
+ const selectTip = document.getElementById('select-tip')
+ const percentage = document.getElementById('percentage')
  const numOfPeople = document.getElementById('num-of-people')
  const tipPerPerson = document.getElementById('per-person')
  const tipTotal = document.getElementById('total')
  const reset = document.getElementById('reset-button')
 
  bill.value = 0.00
+ numOfPeople.value = 0
  
- tip.onclick = () => {
-     console.log(bill.value - 1)
-     console.log(tip.innerText)
-     console.log(bill.value/parseFloat(tip.innerText))
- }
 
 
+ document.querySelectorAll('#percentage').forEach(item => {
+    item.addEventListener('focusin', () => {
+        console.log(parseFloat(item.innerText))
+    })
+ })
     
 })
