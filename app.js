5eb89127ee7406670934a63814ee48e4e43d54d2
Aniruddh <ojhaa94@gmail.com>
2021-10-05 19:34:52 +0100

    maths correct, now to communicate with html

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+ 
+ // Variables
  const bill = document.getElementById('bill-input')
  const selectTip = document.getElementById('select-tip')
  const percentage = document.getElementById('percentage')
@@ -6,15 +8,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
  const tipPerPerson = document.getElementById('per-person')
  const tipTotal = document.getElementById('total')
  const reset = document.getElementById('reset-button')
-
+ 
+ // Default Values
  bill.value = 0.00
  numOfPeople.value = 0
  
+ // Functions
+ function calculateTipTotal(bill, tip) {
+     return bill * (tip/100) 
+ }
 
-
+ // Event Listeners
  document.querySelectorAll('#percentage').forEach(item => {
     item.addEventListener('focusin', () => {
-        console.log(parseFloat(item.innerText))
+        let tip = parseFloat(item.innerText)
+        console.log(calculateTipTotal(bill.value, tip))
+        console.log(calculateTipTotal(bill.value, tip)/numOfPeople.value)
     })
  })
     
