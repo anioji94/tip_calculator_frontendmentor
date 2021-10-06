4d518d8061ae787fe116fcb04c71dd02a32fff0a
Aniruddh <ojhaa94@gmail.com>
2021-10-06 09:09:52 +0100

    maths now assigned to correct html elements

--- app.js
+++ app.js
@@ -5,26 +5,44 @@ document.addEventListener('DOMContentLoaded', (event) => {
  const selectTip = document.getElementById('select-tip')
  const percentage = document.getElementById('percentage')
  const numOfPeople = document.getElementById('num-of-people')
- const tipPerPerson = document.getElementById('per-person')
- const tipTotal = document.getElementById('total')
+ const tipPerPerson = document.getElementById('per-person-value')
+ const tipTotal = document.getElementById('total-value')
  const reset = document.getElementById('reset-button')
  
  // Default Values
  bill.value = 0.00
  numOfPeople.value = 0
+ tipPerPerson.innerText = 0.00
+ tipTotal.innerText = 0.00
  
+
  // Functions
  function calculateTipTotal(bill, tip) {
      return bill * (tip/100) 
  }
 
+ function resetValue() {
+    bill.value = 0.00
+    numOfPeople.value = 0
+    tipPerPerson.innerText = 0.00
+    tipTotal.innerText = 0.00
+ }
+
  // Event Listeners
  document.querySelectorAll('#percentage').forEach(item => {
     item.addEventListener('focusin', () => {
+        // resetValue()
         let tip = parseFloat(item.innerText)
-        console.log(calculateTipTotal(bill.value, tip))
-        console.log(calculateTipTotal(bill.value, tip)/numOfPeople.value)
+        tipTotal.innerText = calculateTipTotal(bill.value, tip).toFixed(2)
+        tipPerPerson.innerText = (calculateTipTotal(bill.value, tip)/numOfPeople.value).toFixed(2)
     })
  })
+
+ reset.onclick = () => {
+    bill.value = 0.00
+    numOfPeople.value = 0
+    tipPerPerson.innerText = 0.00
+    tipTotal.innerText = 0.00 
+ }
     
 })
